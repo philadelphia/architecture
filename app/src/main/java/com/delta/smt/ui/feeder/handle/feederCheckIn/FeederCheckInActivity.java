3d028445ec20213fbb7febdde015723063231022
tao.zt.zhang <tao.zt.zhang@deltaww.com>
2017-01-06 11:00:59 +0800

    Tune UI

--- app/src/main/java/com/delta/smt/ui/feeder/handle/feederCheckIn/FeederCheckInActivity.java
+++ app/src/main/java/com/delta/smt/ui/feeder/handle/feederCheckIn/FeederCheckInActivity.java
@@ -2,13 +2,15 @@ package com.delta.smt.ui.feeder.handle.feederCheckIn;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.delta.smt.R;
 
 public class FeederCheckInActivity extends AppCompatActivity {
-
+    private static final String TAG = "FeederCheckInActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "onCreate: ");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feeder_check_in);
     }
