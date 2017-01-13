0d8c1ffa1fe511e40205ca28c8d5140a93fd2040
zhenyu.liu <liuzhenyu1531@163.com>
2017-01-13 17:08:29 +0800

    退入主仓库对接

--- app/src/mock/java/com/delta/smt/di/module/ServiceModule.java
+++ app/src/mock/java/com/delta/smt/di/module/ServiceModule.java
@@ -9,7 +9,7 @@ import com.delta.smt.entity.FeederSupplyWarningItem;
 import com.delta.smt.entity.ListWarning;
 import com.delta.smt.entity.LoginResult;
 import com.delta.smt.entity.MantissaWarehouseDetails;
-import com.delta.smt.entity.MantissaWarehousePutstorage;
+import com.delta.smt.entity.MantissaWarehousePutstorageResult;
 import com.delta.smt.entity.MantissaWarehouseReady;
 import com.delta.smt.entity.MantissaWarehouseReturn;
 import com.delta.smt.entity.ModuleDownDetailsItem;
@@ -194,23 +194,8 @@ public class ServiceModule {
             }
 
             @Override
-            public Observable<List<MantissaWarehousePutstorage>> getMantissaWarehousePutstorage() {
-                List<MantissaWarehousePutstorage> datas = new ArrayList<>();
-                datas.add(new MantissaWarehousePutstorage("201512121234", "0351234701", "D33E02-02", "-", "等待退入"));
-                datas.add(new MantissaWarehousePutstorage("201512121234", "0351234701", "D33E02-02", "-", "等待退入"));
-                datas.add(new MantissaWarehousePutstorage("201512121234", "0351234701", "D33E02-02", "-", "等待退入"));
-                datas.add(new MantissaWarehousePutstorage("201512121235", "0351234702", "D33E02-02", "", "等待退入"));
-                datas.add(new MantissaWarehousePutstorage("201512121236", "0351234703", "D33E02-02", "", "等待退入"));
-                return Observable.just(datas);
-            }
-
-            @Override
-            public Observable<List<MantissaWarehousePutstorage>> getBeginput() {
-                List<MantissaWarehousePutstorage> datas = new ArrayList<>();
-                datas.add(new MantissaWarehousePutstorage("201512121234", "0351234701", "D33E02-02", "-", "等待退入"));
-                datas.add(new MantissaWarehousePutstorage("201512121234", "0351234701", "D33E02-02", "-", "等待退入"));
-                datas.add(new MantissaWarehousePutstorage("201512121234", "0351234701", "D33E02-02", "T-001", "开始退库"));
-                return Observable.just(datas);
+            public Observable<MantissaWarehousePutstorageResult> getMantissaWarehousePutstorage() {
+                return null;
             }
 
             //故障预警
@@ -455,7 +440,7 @@ public class ServiceModule {
 
             @Override
             public Observable<Result> upLoadFeederSupplyResult() {
-                return Observable.just(new Result("success", "OK"));
+                return null;
             }
 
             @Override
