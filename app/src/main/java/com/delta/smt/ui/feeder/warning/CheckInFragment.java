0a0fb0a663d2a27fb91c3f49e0db208d77d58d18
tao.zt.zhang <tao.zt.zhang@deltaww.com>
2017-01-20 18:06:23 +0800

    与ＤＧ３后台联调

--- app/src/main/java/com/delta/smt/ui/feeder/warning/CheckInFragment.java
+++ app/src/main/java/com/delta/smt/ui/feeder/warning/CheckInFragment.java
@@ -59,6 +59,8 @@ public class CheckInFragment extends BaseFragment<CheckInPresenter> implements C
     private String mCurrentLocation;
     private boolean flag1;
     private boolean flag2;
+    private int index = -1;
+
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
@@ -77,15 +79,8 @@ public class CheckInFragment extends BaseFragment<CheckInPresenter> implements C
         CommonBaseAdapter<FeederCheckInItem> adapterTitle = new CommonBaseAdapter<FeederCheckInItem>(getContext(), dataList) {
             @Override
             protected void convert(CommonViewHolder holder, FeederCheckInItem item, int position) {
-//                holder.setText(R.id.tv_workItemID, "工单ID: " + item.getWorkItemID());
-//                holder.setText(R.id.tv_feederID, "FeederID: " + item.getFeederID());
-//                holder.setText(R.id.tv_materialID, "料号: " + item.getMaterialID());
-//                holder.setText(R.id.tv_location, "架位: " + item.getLocation());
-//                holder.setText(R.id.tv_chkinTimestamp, "入库时间: " + item.getCheckInTimeStamp());
-//                holder.setText(R.id.tv_status, "状态: " + item.getStatus());
                 holder.itemView.setBackgroundColor(Color.GRAY);
-
-            }
+           }
 
             @Override
             protected int getItemViewLayoutId(int position, FeederCheckInItem item) {
@@ -107,6 +102,13 @@ public class CheckInFragment extends BaseFragment<CheckInPresenter> implements C
                 holder.setText(R.id.tv_usedTime, item.getUsed_time());
                 holder.setText(R.id.tv_chkinTimestamp, item.getCheckInTimeStamp());
                 holder.setText(R.id.tv_status, item.getStatus() == 0 ? "等待入库" :"已经入库");
+
+                if (position == index) {
+                    holder.itemView.setBackgroundColor(Color.YELLOW);
+                } else {
+                    holder.itemView.setBackgroundColor(Color.WHITE);
+                }
+
             }
 
             @Override
@@ -162,79 +164,6 @@ public class CheckInFragment extends BaseFragment<CheckInPresenter> implements C
 
     }
 
-//    @Override
-//    public void onScanSuccess(String barcode) {
-//        Log.i(TAG, "onScanSuccess: ");
-//        Log.i(TAG, "barcode == " + barcode);
-//        BarCodeType codeType = BarCodeUtils.barCodeType(barcode);
-//        Log.i(TAG, "codeType:== " + codeType);
-//        if (!TextUtils.isEmpty(barcode)) {
-//            if (codeType != null) {
-//                switch (codeType) {
-//                    case MATERIAL_BLOCK_BARCODE: //料号
-//                        Log.i(TAG, "料号: ");
-//                        try {
-//                            MaterialBlockBarCode materialBlockBarCode = (MaterialBlockBarCode) new BarCodeParseIpml().getEntity(barcode,BarCodeType.MATERIAL_BLOCK_BARCODE);
-//                            if (null != materialBlockBarCode){
-//                                mCurrentMaterialID = materialBlockBarCode.getDeltaMaterialNumber();
-//                                mCurrentSerialNumber = materialBlockBarCode.getStreamNumber();
-//                                Log.i(TAG, "mCurrentMaterialID: " + mCurrentMaterialID) ;
-//                                Log.i(TAG, "mCurrentSerialNumber: " + mCurrentSerialNumber) ;
-//                            }
-//                            for (FeederCheckInItem feederCheckInItem : dataSource) {
-//                                if (materialBlockBarCode.getDeltaMaterialNumber().equalsIgnoreCase(feederCheckInItem.getMaterialID()) && materialBlockBarCode.getStreamNumber().equalsIgnoreCase(feederCheckInItem.getSerial_num()) ) {
-//                                    Log.i(TAG, "对应的feederCheckInItem: " + feederCheckInItem.toString());
-//                                    dataSource.set(0, feederCheckInItem);
-//                                    adapter.notifyDataSetChanged();
-//                                    Log.i(TAG, "onScanSuccess: " );
-//                                    Map<String, String> map = new HashMap<>();
-//                                    map.put("material_num", materialBlockBarCode.getDeltaMaterialNumber());
-//                                    map.put("serial_num", materialBlockBarCode.getStreamNumber());
-//                                    Gson gson = new Gson();
-//                                    String argument = gson.toJson(map);
-//                                    Log.i(TAG, "argument== " + argument);
-//                                    getPresenter().getFeederLocation(argument);
-//                                }
-//                            }
-//                        } catch (EntityNotFountException e) {
-//                            e.printStackTrace();
-//                        }
-//
-//
-//
-//                        break;
-//                    case FRAME_LOCATION: //架位ID
-//                        FrameLocation frameLocation = null;
-//                        try {
-//                            frameLocation = (FrameLocation) new BarCodeParseIpml().getEntity(barcode, BarCodeType.FRAME_LOCATION);
-//                            if (null != frameLocation){
-//                                mCurrentLocation = frameLocation.getSource();
-//                                Log.i(TAG, "onScanSuccess: " + frameLocation.toString());
-//                                Map<String, String> map = new HashMap<>();
-//                                map.put("material_num", mCurrentMaterialID);
-//                                map.put("serial_num", mCurrentSerialNumber);
-//                                map.put("position", mCurrentLocation);
-//                                Gson gson = new Gson();
-//                                String argument = gson.toJson(map);
-//                                Log.i(TAG, "argument== " + argument);
-//                                getPresenter().getFeederCheckInTime(argument);
-//                            }
-//                        } catch (EntityNotFountException e) {
-//                            e.printStackTrace();
-//                        }
-//
-//                        break;
-//
-//                    default:
-//                        break;
-//
-//                }
-//            }
-//        }
-//
-//    }
-
-
     @Override
     public void onScanSuccess(String barcode) {
         Log.i(TAG, "onScanSuccess: ");
@@ -251,6 +180,7 @@ public class CheckInFragment extends BaseFragment<CheckInPresenter> implements C
                 Log.i(TAG, "mCurrentSerialNumber: " + mCurrentSerialNumber) ;
                 for (FeederCheckInItem feederCheckInItem : dataSource) {
                     if (mCurrentMaterialID.equalsIgnoreCase(feederCheckInItem.getMaterialID()) && mCurrentSerialNumber.equalsIgnoreCase(feederCheckInItem.getSerial_num()) ) {
+                        index = dataSource.indexOf(feederCheckInItem);
                         Log.i(TAG, "对应的feederCheckInItem: " + feederCheckInItem.toString());
                         dataSource.set(0, feederCheckInItem);
                         adapter.notifyDataSetChanged();
