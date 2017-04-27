c514520c465ece4be9b9f6e30ed57d8d2ceed69a
tao.zt.zhang <tao.zt.zhang@deltaww.com>
2017-04-27 11:36:47 +0800

    修改Feeder模块数据请求部分RxAndroid的实现

--- app/src/main/java/com/delta/smt/ui/feeder/warning/supply/mvp/SupplyPresenter.java
+++ app/src/main/java/com/delta/smt/ui/feeder/warning/supply/mvp/SupplyPresenter.java
@@ -2,6 +2,8 @@ package com.delta.smt.ui.feeder.warning.supply.mvp;
 
 import com.delta.commonlibs.base.mvp.BasePresenter;
 import com.delta.commonlibs.di.scope.ActivityScope;
+import com.delta.commonlibs.rx.rxerrorhandler.RxErrorHandler;
+import com.delta.commonlibs.rx.rxerrorhandler.RxErrorHandlerSubscriber;
 import com.delta.smt.entity.FeederSupplyWarningItem;
 import com.delta.smt.entity.Result;
 
@@ -16,43 +18,40 @@ import rx.functions.Action1;
  */
 
 @ActivityScope
-public class SupplyPresenter extends BasePresenter<SupplyContract.Model, SupplyContract.View>{
+public class SupplyPresenter extends BasePresenter<SupplyContract.Model, SupplyContract.View> {
+
+    private RxErrorHandler rxErrorHandler;
+
     @Inject
-     SupplyPresenter(SupplyContract.Model model, SupplyContract.View mView) {
+    SupplyPresenter(SupplyContract.Model model, SupplyContract.View mView, RxErrorHandler rxErrorHandler) {
         super(model, mView);
+        this.rxErrorHandler = rxErrorHandler;
     }
 
 
     //获取Feeder备料排程
-    public void getAllSupplyWorkItems(){
-        getModel().getAllSupplyWorkItems().doOnSubscribe(new Action0() {
+    public void getAllSupplyWorkItems() {
+        getModel().getAllSupplyWorkItems().subscribe(new RxErrorHandlerSubscriber<Result<FeederSupplyWarningItem>>(rxErrorHandler) {
             @Override
-            public void call() {
+            public void onStart() {
+                super.onStart();
                 getView().showLoadingView();
             }
-        }).subscribe(new Action1<Result<FeederSupplyWarningItem>>() {
+
             @Override
-            public void call(Result<FeederSupplyWarningItem> feederSupplyWorkItems) {
-                if ("0".equals(feederSupplyWorkItems.getCode())) {
-                    if (feederSupplyWorkItems.getRows().size() == 0) {
+            public void onNext(Result<FeederSupplyWarningItem> feederSupplyWarningItemResult) {
+                if ("0".equals(feederSupplyWarningItemResult.getCode())) {
+                    if (feederSupplyWarningItemResult.getRows().size() == 0) {
                         getView().showEmptyView();
                     } else {
                         getView().showContentView();
-                        getView().onSuccess(feederSupplyWorkItems.getRows());
+                        getView().onSuccess(feederSupplyWarningItemResult.getRows());
                     }
-                }
-            }
-        }, new Action1<Throwable>() {
-            @Override
-            public void call(Throwable throwable) {
-
-                try {
-                    getView().onFailed(throwable.getMessage());
+                } else {
+                    getView().onFailed(feederSupplyWarningItemResult.getMessage());
                     getView().showErrorView();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
 
+                }
             }
         });
     }
